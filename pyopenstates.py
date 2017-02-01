070c47529e562b4f273fb72115d3ec31d99a1b64
Sean Whalen <whalenster@gmail.com>
2017-02-01 15:34:56 -0500

    1.0.3 - Bump version to fix README URL in PyPI

--- pyopenstates.py
+++ pyopenstates.py
@@ -24,7 +24,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License."""
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 API_ROOT = "https://openstates.org/api/v1/"
 DEFUALT_USER_AGENT = "pyopenstates/{0}".format(__version__)
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 description = "A client for the Open States API"
 
 
