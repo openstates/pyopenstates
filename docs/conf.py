06180ae3b26b648d7a6906fca966092f8ce90ff4
James Turk <james.p.turk@gmail.com>
2017-02-02 10:57:57 -0500

    drop old extensions

--- docs/conf.py
+++ docs/conf.py
@@ -35,9 +35,7 @@ from pyopenstates import __version__
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-              #'sphinx.ext.githubpages',
               'sphinx.ext.napoleon',
-              #'sphinxcontrib.fulltoc'
               ]
 
 # Add any paths that contain templates here, relative to this directory.
