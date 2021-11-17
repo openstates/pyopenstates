d49012ba1c56deecedb3ac65b374c5951ba2017c
James Turk <dev@jamesturk.net>
2021-11-17 14:54:11 -0500

    2.1.0

--- src/pyopenstates/config.py
+++ src/pyopenstates/config.py
@@ -1,6 +1,6 @@
 import os
 
-__version__ = "2.0.0"
+__version__ = "2.1.0"
 API_ROOT = "https://v3.openstates.org"
 DEFAULT_USER_AGENT = f"pyopenstates/{__version__}"
 API_KEY_ENV_VAR = "OPENSTATES_API_KEY"
